beeeeb54f423cf46d9509ac60a55c0f8235678e0
Ricardo Yang <ricardo.yang00@gmail.com>
2024-03-25 22:25:37 +0000

    style for search medication button

--- app/lib/screens/stock_screen.dart
+++ app/lib/screens/stock_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:app/screens/search_screen.dart';
 
 class StockScreen extends StatelessWidget {
-  const StockScreen({super.key});
+  const StockScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +10,27 @@ class StockScreen extends StatelessWidget {
       backgroundColor: Color.fromRGBO(255, 244, 236, 1),
       body: Stack(
         children: [
-          Center(
-            child: IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                Navigator.push(
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child:
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => SearchScreen()),
-                );
-              },
-            )
+                  );
+                },
+                icon: Icon(Icons.search),
+                label: Text('Search Medication'),
+                style: ElevatedButton.styleFrom(
+                    elevation: 4,
+                    shadowColor: Colors.black.withOpacity(0.5),
+                    backgroundColor: Color.fromRGBO(255, 220, 194, 1),
+                    foregroundColor: Color.fromRGBO(225, 95, 0, 1)
+                ),
+              ),
+            ),
           ),
           Positioned(
             left: -206,
