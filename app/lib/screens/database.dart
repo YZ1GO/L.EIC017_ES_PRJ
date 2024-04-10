82aef065d4b610e275a467dda569f800235f1c46
Ricardo Yang <ricardo.yang00@gmail.com>
2024-04-10 16:21:08 +0100

    added search feature on database

--- app/lib/screens/database.dart
+++ app/lib/screens/database.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class DatabaseContentScreen extends StatelessWidget {
+class DatabaseContentScreen extends StatefulWidget {
+  @override
+  _DatabaseContentScreenState createState() => _DatabaseContentScreenState();
+}
+
+class _DatabaseContentScreenState extends State<DatabaseContentScreen> {
+  TextEditingController _searchController = TextEditingController();
+  List<Map<dynamic, dynamic>> _filteredBrandList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Database Content'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: BrandSearchDelegate(brandList: _filteredBrandList),
+              );
+            },
+          ),
+        ],
       ),
       body: StreamBuilder(
         stream: FirebaseDatabase.instance.reference().child('brands').onValue,
@@ -29,11 +48,14 @@ class DatabaseContentScreen extends StatelessWidget {
           // Convert List<dynamic> to List<Map<dynamic, dynamic>>
           List<Map<dynamic, dynamic>> brandList = List<Map<dynamic, dynamic>>.from(brandsData);
 
-          // Build UI from brandList
+          // Set filteredBrandList initially to the full brand list
+          _filteredBrandList = brandList;
+
+          // Build UI from _filteredBrandList
           return ListView.builder(
-            itemCount: brandList.length,
+            itemCount: _filteredBrandList.length,
             itemBuilder: (BuildContext context, int index) {
-              var brand = brandList[index];
+              var brand = _filteredBrandList[index];
               return ListTile(
                 title: Text(brand['brand_name'] ?? ''),
                 subtitle: Text(
@@ -47,3 +69,67 @@ class DatabaseContentScreen extends StatelessWidget {
     );
   }
 }
+
+class BrandSearchDelegate extends SearchDelegate<String> {
+  final List<Map<dynamic, dynamic>> brandList;
+
+  BrandSearchDelegate({required this.brandList});
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  Widget _buildSearchResults(BuildContext context) {
+    final List<Map<dynamic, dynamic>> filteredBrands = _filterBrands();
+
+    return ListView.builder(
+      itemCount: filteredBrands.length,
+      itemBuilder: (BuildContext context, int index) {
+        var brand = filteredBrands[index];
+        return ListTile(
+          title: Text(brand['brand_name'] ?? ''),
+          subtitle: Text(
+            'Form: ${brand['form'] ?? ''}, Strength: ${brand['strength'] ?? ''}, Price: ${brand['price'] ?? ''}',
+          ),
+        );
+      },
+    );
+  }
+
+  List<Map<dynamic, dynamic>> _filterBrands() {
+    return query.isEmpty
+        ? brandList
+        : brandList
+        .where((brand) => brand['brand_name'].toString().toLowerCase().contains(query.toLowerCase()))
+        .toList();
+  }
+}
+
--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/calendar_widget.dart';
 import '../widgets/system_notification_test_widget.dart';
-import 'database.dart';
 import '../widgets/eclipse_background.dart';
+import 'database.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key});
@@ -21,47 +21,46 @@ class HomeScreenState extends State<HomeScreen> {
           eclipse_background(),
           const CalendarWidget(),
           Positioned(
-              left: 0,
-              right: 0,
-              top: 400,
-              child:
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () async {
-                      await requestNotificationPermission(context);
-                    },
-                    icon: Icon(Icons.notifications_active),
-                    label: Text('Test notification'),
-                    style: ElevatedButton.styleFrom(
-                      elevation: 4,
-                      shadowColor: Colors.black.withOpacity(0.5),
-                      backgroundColor: Color.fromRGBO(255, 131, 41, 1),
-                      foregroundColor: Colors.white,
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    ),
+            left: 0,
+            right: 0,
+            top: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    await requestNotificationPermission(context);
+                  },
+                  icon: Icon(Icons.notifications_active),
+                  label: Text('Test notification'),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 4,
+                    shadowColor: Colors.black.withOpacity(0.5),
+                    backgroundColor: Color.fromRGBO(255, 131, 41, 1),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   ),
-                  SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DatabaseContentScreen()),
-                      );
-                    },
-                    icon: Icon(Icons.cloud),
-                    label: Text('Firebase Database'),
-                    style: ElevatedButton.styleFrom(
-                      elevation: 4,
-                      shadowColor: Colors.black.withOpacity(0.5),
-                      backgroundColor: Color.fromRGBO(255, 220, 194, 1),
-                      foregroundColor: Color.fromRGBO(225, 95, 0, 1),
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DatabaseContentScreen()),
+                    );
+                  },
+                  icon: Icon(Icons.cloud),
+                  label: Text('Firebase Database'),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 4,
+                    shadowColor: Colors.black.withOpacity(0.5),
+                    backgroundColor: Color.fromRGBO(255, 220, 194, 1),
+                    foregroundColor: Color.fromRGBO(225, 95, 0, 1),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
